58fcbaa47c043956fbfbc49bc40fb4e7590e2400
mwintherDK <maha@aspit.dk>
2019-06-19 08:28:28 +0200

    Updated a few places for fewer lines - also set form eventlistener to listen for submit event instead of click

--- js/script.js
+++ js/script.js
@@ -3,28 +3,27 @@ let select = document.querySelector("select");
 let jokeHere = document.querySelector(".jokeHere");
 let jokePlaceholder = "";
 
-form.addEventListener("click", function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (e.target.matches("input[type='submit']")) {
-    getJokes(select.value);
-  }
+  //select.value er det antal vitser brugeren vil ha'
+  getJokes(select.value);
 });
 
 function getJokes(number) {
   fetch("http://api.icndb.com/jokes/random/" + number)
     //Nedenstående kan osse skrives: .then(response => response.json())
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       jokePlaceholder += "<ul>";
       data.value.forEach(element => {
-        jokePlaceholder += "<li>" + element.joke + "</li>";
+        jokePlaceholder += `<li> ${element.joke} </li>`;
       });
       jokePlaceholder += "</ul>";
       jokeHere.innerHTML = jokePlaceholder;
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log("Noget her: " + err);
     });
 }
